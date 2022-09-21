18fce76b1a82d7e580a1020abaeca9d4a30c0ee8
Pranil Takawane <99954777+Pranil9699@users.noreply.github.com>
2022-09-21 15:48:55 +0530

    Update QueryNth_Salary.sql

--- QueryNth_Salary.sql
+++ QueryNth_Salary.sql
@@ -1,1 +1,2 @@
 select name from emp e1 where 3-1 = (select COUNT (DISTINCT salary) from emp e2 where e2.salary>e1.salary);
+https://user-images.githubusercontent.com/99954777/185356323-005b4d59-0494-44f2-acab-f5abca23c5e9.jpg
